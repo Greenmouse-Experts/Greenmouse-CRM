723032a9ef2bfd4571f88534cf28336e2941a1c0
Promise <promiseezema11@gmail.com>
2023-03-14 14:25:47 +0100

    updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -467,14 +467,14 @@ class AdminController extends Controller
     public function update_client_management($id, Request $request) 
     {
         //Validate Request
-        $this->validate($request, [
-            'title' => ['required', 'string', 'max:255'],
-            'first_name' => ['required', 'string', 'max:255'],
-            'last_name' => ['required', 'string', 'max:255'],
-            'address_1' => ['required', 'string'],
-            'phone_number' => ['required', 'numeric'],
-            'email' => ['required', 'string'],
-        ]);
+        // $this->validate($request, [
+        //     'title' => ['required', 'string', 'max:255'],
+        //     'first_name' => ['required', 'string', 'max:255'],
+        //     'last_name' => ['required', 'string', 'max:255'],
+        //     'address_1' => ['required', 'string'],
+        //     'phone_number' => ['required', 'numeric'],
+        //     'email' => ['required', 'string'],
+        // ]);
 
         $Finder = Crypt::decrypt($id);
 
