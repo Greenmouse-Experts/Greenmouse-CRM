b3c7efa04ea3f5e14817191e39ad971f77fb1791
Promise <promiseezema11@gmail.com>
2023-08-07 13:58:52 +0100

    updated

--- resources/views/admin/financial/edit.blade.php
+++ resources/views/admin/financial/edit.blade.php
@@ -15,7 +15,7 @@
         <div class="container-fluid">
             <div class="app-title">
                 <div>
-                    <h1><i class="fa fa-list-alt"></i> Edit Debtors</h1>
+                    <h1><i class="fa fa-list-alt"></i> Edit Sales</h1>
                 </div>
                 <ul class="app-breadcrumb breadcrumb">
                     <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
